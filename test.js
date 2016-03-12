4c18a67d6c08d61eb314b07a5e12ac9cb0cc4266
Matteo Collina <hello@matteocollina.com>
2016-03-12 22:36:22 +0100

    Test refactoring.

--- test.js
+++ test.js
@@ -25,54 +25,75 @@ function getKey (instance) {
   return key
 }
 
+function boot (t, parent, cb) {
+  if (typeof parent === 'function') {
+    cb = parent
+    parent = null
+  }
+
+  const base = []
+  if (parent) {
+    base.push(parent.whoami())
+  }
+
+  const instance = upring(opts({
+    base: base
+  }))
+
+  t.tearDown(instance.close.bind(instance))
+
+  instance.on('up', () => {
+    cb(instance)
+  })
+}
+
+function bootTwo (t, cb) {
+  boot(t, (i1) => {
+    t.pass('i1 up')
+    boot(t, i1, (i2) => {
+      t.pass('i2 up')
+      cb(i1, i2)
+    })
+  })
+}
+
 test('request to two nodes', { timeout: 5000 }, (t) => {
   t.plan(10)
 
-  const i1 = upring(opts())
-  t.tearDown(i1.close.bind(i1))
-  i1.on('up', () => {
-    t.pass('i1 up')
-    const i2 = upring(opts({
-      base: [i1.whoami()]
-    }))
-    t.tearDown(i2.close.bind(i2))
-    i2.on('up', () => {
-      t.pass('i2 up')
+  bootTwo(t, (i1, i2) => {
+    let i1Key = getKey(i1)
+    let i2Key = getKey(i2)
 
-      let i1Key = getKey(i1)
-      let i2Key = getKey(i2)
+    i1.request({
+      key: i2Key,
+      hello: 42
+    }, (err, response) => {
+      t.error(err)
+      t.deepEqual(response, {
+        replying: 'i2'
+      }, 'response matches')
+    })
 
-      i1.request({
-        key: i2Key,
-        hello: 42
-      }, (err, response) => {
-        t.error(err)
-        t.deepEqual(response, {
-          replying: 'i2'
-        }, 'response matches')
-      })
+    i2.request({
+      key: i1Key,
+      hello: 42
+    }, (err, response) => {
+      t.error(err)
+      t.deepEqual(response, {
+        replying: 'i1'
+      }, 'response matches')
+    })
 
-      i2.request({
-        key: i1Key,
-        hello: 42
-      }, (err, response) => {
-        t.error(err)
-        t.deepEqual(response, {
-          replying: 'i1'
-        }, 'response matches')
-      })
+    i1.on('request', (req, reply) => {
+      t.equal(req.key, i1Key, 'key matches')
+      t.equal(req.hello, 42, 'other key matches')
+      reply(null, { replying: 'i1' })
+    })
 
-      i1.on('request', (req, reply) => {
-        t.equal(req.key, i1Key, 'key matches')
-        t.equal(req.hello, 42, 'other key matches')
-        reply(null, { replying: 'i1' })
-      })
-
-      i2.on('request', (req, reply) => {
-        t.equal(req.key, i2Key, 'key matches')
-        t.equal(req.hello, 42, 'other key matches')
-        reply(null, { replying: 'i2' })
-      })
+    i2.on('request', (req, reply) => {
+      t.equal(req.key, i2Key, 'key matches')
+      t.equal(req.hello, 42, 'other key matches')
+      reply(null, { replying: 'i2' })
     })
   })
 })
@@ -80,41 +101,30 @@ test('request to two nodes', { timeout: 5000 }, (t) => {
 test('streams!', { timeout: 5000 }, (t) => {
   t.plan(7)
 
-  const i1 = upring(opts())
-  t.tearDown(i1.close.bind(i1))
-  i1.on('up', () => {
-    t.pass('i1 up')
-    const i2 = upring(opts({
-      base: [i1.whoami()]
-    }))
-    t.tearDown(i2.close.bind(i2))
-    i2.on('up', () => {
-      t.pass('i2 up')
+  bootTwo(t, (i1, i2) => {
+    let i2Key = getKey(i2)
 
-      let i2Key = getKey(i2)
+    i1.request({
+      key: i2Key,
+      hello: 42
+    }, (err, response) => {
+      t.error(err)
+      t.equal(response.replying, 'i2', 'response matches')
+      response.streams$.p
+        .pipe(concat((list) => {
+          t.equal(list.toString(), fs.readFileSync(packageFile).toString())
+        }))
+    })
 
-      i1.request({
-        key: i2Key,
-        hello: 42
-      }, (err, response) => {
-        t.error(err)
-        t.equal(response.replying, 'i2', 'response matches')
-        response.streams$.p
-          .pipe(concat((list) => {
-            t.equal(list.toString(), fs.readFileSync(packageFile).toString())
-          }))
-      })
-
-      i2.on('request', (req, reply) => {
-        t.equal(req.key, i2Key, 'key matches')
-        t.equal(req.hello, 42, 'other key matches')
-        let stream = fs.createReadStream(packageFile)
-        reply(null, {
-          replying: 'i2',
-          streams$: {
-            p: stream
-          }
-        })
+    i2.on('request', (req, reply) => {
+      t.equal(req.key, i2Key, 'key matches')
+      t.equal(req.hello, 42, 'other key matches')
+      let stream = fs.createReadStream(packageFile)
+      reply(null, {
+        replying: 'i2',
+        streams$: {
+          p: stream
+        }
       })
     })
   })
